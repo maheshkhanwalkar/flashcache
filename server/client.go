e288b5e0060dc85f2f41f3760feb68d6b697df61
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-08-08 15:57:08 -0400

    client: setup initial accept loop

--- server/client.go
+++ server/client.go
@@ -1,14 +1,38 @@
 package server
 
+import "net"
+
 type ClientManager struct {
 	config *ServerConfig
 }
 
 func (srv *ClientManager) Start() error {
-	// TODO
+	lst, err := makeListener(srv.config)
+
+	if err != nil {
+		return err
+	}
+
+	for {
+		conn, err := lst.Accept()
+
+		if err != nil {
+			break
+		}
+
+		go srv.handleClient(conn)
+	}
+
+	_ = lst.Close()
 	return nil
 }
 
 func (srv *ClientManager) Shutdown() {
 	// TODO
+}
+
+func (srv *ClientManager) handleClient(conn net.Conn) {
+	for {
+		_ = readCommandBytes(conn)
+	}
 }
