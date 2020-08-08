60b8f70944d48d2e9d435f7e6978edecaa597228
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-08-08 11:04:47 -0400

    server: add client manager stub

---
+++ server/client.go
@@ -0,0 +1,14 @@
+package server
+
+type ClientManager struct {
+	config *ServerConfig
+}
+
+func (srv *ClientManager) Start() error {
+	// TODO
+	return nil
+}
+
+func (srv *ClientManager) Shutdown() {
+	// TODO
+}
--- server/server.go
+++ server/server.go
@@ -1,43 +1,27 @@
 package server
 
-import (
-	"strconv"
-)
-
 type Server struct {
 	config *Configuration
+	client *ClientManager
 }
 
 func NewServer(config *Configuration) *Server {
 	srv := Server{}
+	client := ClientManager{}
+
+	client.config = config.serverConf
 	srv.config = config
+	srv.client = &client
 
 	return &srv
-}
-
-// Get the correct address string for the specified connection type
-//   - For Unix domain sockets, the raw address is the final address
-//   - For TCP sockets, the address should be rawAddress:port
-func getAddressString(cType ConnType, rawAddress string, port int) string {
-	var address string
-
-	switch cType {
-	case TCPv4, TCPv6:
-		address = rawAddress + ":" + strconv.Itoa(port)
-	case Unix:
-		address = rawAddress
-	}
-
-	return address
 }
 
 // Start the server, returning an error if it is forced to quit in
 // an unexpected manner
 func (srv *Server) Start() error {
-	// TODO
-	return nil
+	return srv.client.Start()
 }
 
 func (srv *Server) Shutdown() {
-	// TODO
+	srv.client.Shutdown()
 }
