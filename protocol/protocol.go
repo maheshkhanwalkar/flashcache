2a05cce97dd01bb71b7051dcc0727c751b082c22
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-08-15 14:46:33 -0400

    protocol: change ReadCommand and WriteCommand signature

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,13 +1,15 @@
 package protocol
 
-import "net"
-
-func ReadCommand(conn net.Conn) (*Command, error) {
+// Read a command from the input buffer and return the constructed object along with
+// the number of bytes consumed. Returns an error if the input is invalid or not long enough
+func ReadCommand(buffer []byte) (*Command, int, error) {
 	// TODO
-	return nil, nil
+	return nil, 0, nil
 }
 
-func WriteCommand(cmd *Command, conn net.Conn) error {
+// Convert the provided command into its raw byte form, which can be written over the network
+// and parsed back into its original object form
+func WriteCommand(cmd *Command) []byte {
 	// TODO
 	return nil
 }
