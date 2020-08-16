26f1989e0fc6886fc47271b683db4f76d3e14f09
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-08-15 22:17:45 -0400

    protocol: implement WriteCommand

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,6 +1,13 @@
 package protocol
 
-import "errors"
+import (
+	"encoding/binary"
+	"errors"
+)
+
+const (
+	MaxKeySize int = 256
+)
 
 // Read a command from the input buffer and return the constructed object along with
 // the number of bytes consumed. Returns an error if the input is invalid or not long enough
@@ -12,13 +19,43 @@ func ReadCommand(buffer []byte) (*Command, int, error) {
 // Convert the provided command into its raw byte form, which can be written over the network
 // and parsed back into its original object form
 func WriteCommand(cmd *Command) ([]byte, error) {
-	// TODO
-	return nil, nil
+	cb, err := getCommandByte(cmd.tp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var keySize = len(cmd.key)
+
+	if keySize > MaxKeySize || keySize < 0 {
+		return nil, errors.New("provided key is too long or negative")
+	}
+
+	var bufSize = 1 + 2 + keySize
+	var buffer = make([]byte, bufSize)
+
+	// Command byte, key size, key
+	buffer[0] = cb
+	binary.LittleEndian.PutUint16(buffer[1:], uint16(bufSize))
+	copy(buffer[2:], cmd.key)
+
+	if cmd.tp == PUT {
+		op, err := getOpByte(cmd.value)
+
+		if err != nil {
+			return nil, err
+		}
+
+		buffer = append(buffer, op)
+		buffer = appendOp(buffer, cmd.value)
+	}
+
+	return buffer, nil
 }
 
 // Get the command segment value for the given command type
 // Returns an error if the command type is invalid
-func getCommandSegment(tp CommandType) (byte, error) {
+func getCommandByte(tp CommandType) (byte, error) {
 	switch tp {
 	case GET:
 		return 0, nil
@@ -31,13 +68,30 @@ func getCommandSegment(tp CommandType) (byte, error) {
 
 // Get the operand segment value and size for the given command type
 // Returns an error if the operand value type is invalid
-func getOpSegment(value interface{}) (byte, int, error) {
+func getOpByte(value interface{}) (byte, error) {
 	switch value.(type) {
 	case int:
-		return 0, 4, nil
+		return 0, nil
 	case string:
-		return 1, len(value.(string)), nil
+		return 1, nil
 	default:
-		return -1, 0, errors.New("invalid operand value")
+		return -1, errors.New("invalid operand value")
 	}
+}
+
+// Append the operand to the buffer
+func appendOp(buffer []byte, value interface{}) []byte {
+	switch value.(type) {
+	case int:
+		buffer = append(buffer, 0, 0, 0, 0)
+		binary.LittleEndian.PutUint32(buffer[len(buffer) - 4:], value.(uint32))
+
+	case string:
+		buffer = append(buffer, 0, 0)
+		binary.LittleEndian.PutUint16(buffer[len(buffer) - 2:], uint16(len(value.(string))))
+
+		buffer = append(buffer, value.(string)...)
+	}
+
+	return buffer
 }
