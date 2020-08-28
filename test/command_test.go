fe2638810b2115a88b61c092db3ada58b20920f4
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-08-27 21:33:46 -0400

    test: add tests for empty command operands

--- test/command_test.go
+++ test/command_test.go
@@ -7,6 +7,22 @@ import (
 
 func TestGetReadWrite(t *testing.T) {
 	cmd := protocol.NewGet("my-key")
+	readWrite(cmd, t)
+}
+
+func TestEmptyOperands(t *testing.T) {
+	cmd := protocol.NewGet("")
+	readWrite(cmd, t)
+
+	value, _ := protocol.NewOperand(protocol.STRING, "")
+	cmd = protocol.NewPut("hello-world", value)
+	readWrite(cmd, t)
+
+	cmd = protocol.NewPut("", value)
+	readWrite(cmd, t)
+}
+
+func readWrite(cmd *protocol.Command, t *testing.T) {
 	buffer, err := protocol.WriteCommand(cmd)
 
 	AssertEqual(err, nil, t)
@@ -18,5 +34,12 @@ func TestGetReadWrite(t *testing.T) {
 
 	AssertEqual(cmd.Type(), dup.Type(), t)
 	AssertEqual(cmd.Key(), dup.Key(), t)
-	AssertEqual(cmd.Value(), dup.Value(), t)
+
+	// Check for value equality or both are nil
+	if cmd.Value() != nil && dup.Value() != nil {
+		AssertEqual(cmd.Value().Type(), dup.Value().Type(), t)
+		AssertEqual(cmd.Value().Data(), dup.Value().Data(), t)
+	} else {
+		AssertEqual(cmd.Value(), dup.Value(), t)
+	}
 }
