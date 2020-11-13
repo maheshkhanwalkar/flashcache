ae3428bd9c3d2241d60624ab401f9085146a8375
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-11-12 21:26:38 -0500

    runner: add bare-bones server startup

---
+++ src/main/kotlin/com/revtekk/flashcache/Runner.kt
@@ -0,0 +1,7 @@
+package com.revtekk.flashcache
+
+fun main() {
+    /* TODO: add support for port configuration (and configuration in general) */
+    val server = Server(13331)
+    server.start()
+}
